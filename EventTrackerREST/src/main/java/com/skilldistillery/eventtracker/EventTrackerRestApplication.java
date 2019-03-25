98b75ebdb19980d61be596bc6d9a8685baf3075a
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-03-25 12:41:29 -0600

    Troubleshooting AWS deployment

--- EventTrackerREST/src/main/java/com/skilldistillery/eventtracker/EventTrackerRestApplication.java
+++ EventTrackerREST/src/main/java/com/skilldistillery/eventtracker/EventTrackerRestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EntityScan("com.skilldistillery.eventtracker")
 public class EventTrackerRestApplication extends SpringBootServletInitializer{
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
